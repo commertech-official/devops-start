c0f4987bd975e43721a008696342de567ceb0bff
commertech-repkapi <iloveandhateyou@yandex.ru>
2026-02-06 10:27:06 +0000

    Output arrays

--- python/simple_python/less_1.py
+++ python/simple_python/less_1.py
@@ -1,3 +1,12 @@
-spells = ["Ridikkulus", "Wingardium Leviosa", "Avada Kedavra", "Expecto Patronum", "Nox", "Lumos",]
+# Output of the value via the array index
 
+spells = ["Ridikkulus", "Wingardium Leviosa", "Avada Kedavra", "Expecto Patronum", "Nox", "Lumos",]
 print(spells[3])
+
+quotes = {"Moe":"A wise guy, huh?",
+          "Larry":"Ow!",
+          "Curly":"Nyuk nyuk!", 
+   }
+
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
