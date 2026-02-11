1c1341fc33b8c8c3ae09aef335b9822c0399a686
Archgoat <draakzwarte@gmail.com>
2026-02-11 22:19:25 +0300

    Start learn less_2.py

---
+++ python/simple_python/less_2.py
@@ -0,0 +1,20 @@
+# Numbers
+
+print(bool(True))
+print(bool(1))
+print(bool(45))
+print(bool(-45))
+print(bool(False))
+print(bool(0))
+print(bool(0.0))
+
+# Integer numbers
+
+print(5+8)
+print(90-10)
+print(4*7)
+print(7/2)
+print(7//2)
+print(7%3)
+print(3**4)
+print(divmod(9,5))
