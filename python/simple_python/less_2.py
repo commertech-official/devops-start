747a5c862c22011e2a9cc0f8c7e03616263eedd5
Archgoat <draakzwarte@gmail.com>
2026-02-12 21:53:21 +0300

    Continue learn less_2.py

--- python/simple_python/less_2.py
+++ python/simple_python/less_2.py
@@ -18,3 +18,37 @@ print(7//2)
 print(7%3)
 print(3**4)
 print(divmod(9,5))
+
+# The number system
+
+value = 65
+print(bin(value))
+print(hex(value))
+print(chr(65))
+print(ord('A'))
+
+# Type conversion
+
+print(int(True))
+print(int(False))
+print(bool(1))
+print(bool(0))
+print(int(98.6))
+print(int(1.0e4))
+print(bool(1.0))
+print(bool(0.0))
+print(int('99'))
+print(int('-23'))
+print(int('+12'))
+print(int('1_000_000'))
+print(int('10',2))
+print(int('10',16))
+print(int('10',22))
+print(int(12345))
+# print(int('99 botles of beer'))
+# print(int(''))
+
+# The volume of the data type
+
+googol = 10**100
+print(googol*googol)
