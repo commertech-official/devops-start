a6cf3553244a5a49fb84bb43eeb22262f50a863d
commertech-repkapi <iloveandhateyou@yandex.ru>
2026-03-12 13:21:49 +0000

    Getting started less_4

---
+++ python/simple_python/less_4.py
@@ -0,0 +1,12 @@
+# Work with quotation marks
+
+# Usage various types of quotation marks
+
+print('Usage various types of quotation marks')
+print('Give',"us",'''some''',"""space""")
+
+# Creating strings using the function str()
+
+print('result str(98,6) =', str(98.6))
+print('result str(1.0e4) =', str(1.0e4))
+print('result str(True) =', str(True))
