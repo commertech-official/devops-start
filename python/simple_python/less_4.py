5fb21949fe2f2c07a225b5f1b7d50d0d5b016df2
commertech-repkapi <iloveandhateyou@yandex.ru>
2026-03-30 13:13:04 +0000

    Continued study less_4

--- python/simple_python/less_4.py
+++ python/simple_python/less_4.py
@@ -10,3 +10,32 @@ print('Give',"us",'''some''',"""space""")
 print('result str(98,6) =', str(98.6))
 print('result str(1.0e4) =', str(1.0e4))
 print('result str(True) =', str(True))
+
+# Creating strings with \n
+
+print("'Guten Morgen, mein Herr!'\nsaid mad king Ludwig to his wig.")
+
+# Escape sequences
+
+palindrome = 'A man,\nA plan,\nA canal,\nPanama'
+print(palindrome)
+
+print('\tabc')
+print('a\tbc')
+print('ab\tc')
+print('abc\t')
+
+testimony = "\"I did nothing!\" he said. \"Or that other thing.\""
+print(testimony)
+
+fact = "The world's largest rubber duck was 54'2\" by 65'7\" by 105'"
+print(fact)
+
+speech = 'The backslash (\\) bends over backwards to please you.'
+print(speech)
+
+info = r'Type a \n to get a new line in a normal string'
+info
+
+poem = r'''Boys and girls, come out to play. The moon doth shine as bright as day.'''
+print(poem)
